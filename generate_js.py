1e4db1e77d39f9e8fa2e595907a66484f75a3e2c
Chelsea Pugh <chelsea.pugh93@gmail.com>
2015-03-11 14:47:18 -0400

    adding js generator, need to add drop down selections

---
+++ generate_js.py
@@ -0,0 +1,25 @@
+event_name = raw_input('Enter the event name: ');
+event_date = raw_input('Enter the date (mm/dd/yyyy): ')
+event_time = raw_input('Enter time time (ex. 7pm): ')
+event_location = raw_input('Enter the location: ')
+event_shrt_desc = raw_input('Enter shortened event description (200 chars max): ')
+event_desc = raw_input('Enter the event description: ')
+
+print event_name, event_location
+date = event_date.split("/")
+javascript_code = ""
+
+javascript_code += "document.getElementsByClassName('ticketTitleInput')[0].value = '" + event_name + "';\n"
+javascript_code += "document.getElementById('DATE_S_MonthInput_Event__bDate_S_Month').value = '" + date[0] + "';\n"
+javascript_code += "document.getElementById('DATE_S_DayInput_Event__bDate_S_Day').value = '" + date[1] + "';\n"
+javascript_code += "document.getElementById('DATE_S_YearInput_Event__bDate_S_Year').value = '" + date[2] + "';\n"
+
+javascript_code += "document.getElementById('userfield11').value = '" + event_time + "';\n"
+javascript_code += "document.getElementById('userfield14').value = '" + event_location + "';\n"
+javascript_code += "document.getElementById('userfield7').value = '" + event_shrt_desc + "';\n"
+javascript_code += "document.getElementById('descrpt').value = '" + event_desc + "';\n"
+
+javascript_code += "document.getElementById('userfield16').value = 'Chelsea Pugh';\n"
+
+print "Run this js in the console of the EECS announcement submission page:"
+print javascript_code
