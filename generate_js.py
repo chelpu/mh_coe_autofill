0c1bcd9f48df128ab9706b742900577b193dd015
Chelsea Pugh <chelsea.pugh93@gmail.com>
2015-03-12 15:26:19 -0400

    doing length/formatting checks, adding field selections

--- generate_js.py
+++ generate_js.py
@@ -1,12 +1,21 @@
+import sys
+
 event_name = raw_input('Enter the event name: ');
 event_date = raw_input('Enter the date (mm/dd/yyyy): ')
+date = event_date.split("/")
+if not (len(date) == 3):
+    sys.exit("Date is incorrectly formatted")
+
 event_time = raw_input('Enter time time (ex. 7pm): ')
 event_location = raw_input('Enter the location: ')
 event_shrt_desc = raw_input('Enter shortened event description (200 chars max): ')
+if len(event_shrt_desc) > 200:
+    sys.exit("Short description is more than 200 characters...")
+
 event_desc = raw_input('Enter the event description: ')
 
 print event_name, event_location
-date = event_date.split("/")
+
 javascript_code = ""
 
 javascript_code += "document.getElementsByClassName('ticketTitleInput')[0].value = '" + event_name + "';\n"
@@ -18,6 +27,8 @@ javascript_code += "document.getElementById('userfield11').value = '" + event_ti
 javascript_code += "document.getElementById('userfield14').value = '" + event_location + "';\n"
 javascript_code += "document.getElementById('userfield7').value = '" + event_shrt_desc + "';\n"
 javascript_code += "document.getElementById('descrpt').value = '" + event_desc + "';\n"
+javascript_code += "document.getElementById('userfield8').value = 'Workshop';\n"
+javascript_code += "document.getElementById('userfield17').value = 'Student__bOrganization';\n"
 
 javascript_code += "document.getElementById('userfield16').value = 'Chelsea Pugh';\n"
 
